ffdf373e094ca0ea864b464e9589b26592ab5b0c
lazysong <hfutsong@gmail.com>
2016-05-07 18:47:59 +0800

    实现了对BottomNavigationBar的点击事件的初步响应，并添加了几个fregment的布局文件

--- app/src/main/java/com/example/songhui/bottomnavigationbardemo/MainActivity.java
+++ app/src/main/java/com/example/songhui/bottomnavigationbardemo/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.songhui.bottomnavigationbardemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.ashokvarma.bottomnavigation.BadgeItem;
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationBar.OnTabSelectedListener {
     BottomNavigationBar bottomNavigationBar;
     BadgeItem numberBadgeItem;
 
@@ -22,13 +23,31 @@ public class MainActivity extends AppCompatActivity {
                 .setBackgroundColorResource(R.color.blue)
                 .setText("" + 0)
                 .setHideOnSelect(true);
+        bottomNavigationBar.setTabSelectedListener(this);
+        refresh();
+    }
+
+    void refresh() {
         bottomNavigationBar
                 .addItem(new BottomNavigationItem(R.drawable.ic_home_white_24dp, "Home").setActiveColorResource(R.color.orange).setBadgeItem(numberBadgeItem))
                 .addItem(new BottomNavigationItem(R.drawable.ic_book_white_24dp, "Books").setActiveColorResource(R.color.teal))
                 .addItem(new BottomNavigationItem(R.drawable.ic_music_note_white_24dp, "Music").setActiveColorResource(R.color.blue))
                 .addItem(new BottomNavigationItem(R.drawable.ic_tv_white_24dp, "Movies & TV").setActiveColorResource(R.color.brown))
-                .addItem(new BottomNavigationItem(R.drawable.ic_videogame_asset_white_24dp, "Games").setActiveColorResource(R.color.grey))
                 .setFirstSelectedPosition(0)
                 .initialise();
+    }
+
+    @Override
+    public void onTabSelected(int position) {
+        Toast.makeText(MainActivity.this, "position " + position + "is selected", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onTabUnselected(int position) {
+    }
+
+    @Override
+    public void onTabReselected(int position) {
+        Toast.makeText(MainActivity.this, "position " + position + "is reselected", Toast.LENGTH_SHORT).show();
     }
 }
