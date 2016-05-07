faa7dfd5d791a43f5f8cdd3cd6490532248ae290
lazysong <hfutsong@gmail.com>
2016-05-07 18:29:09 +0800

    初步实现了底部的导航栏UI界面，尚未添加响应事件，成功运行

--- app/src/main/java/com/example/songhui/bottomnavigationbardemo/MainActivity.java
+++ app/src/main/java/com/example/songhui/bottomnavigationbardemo/MainActivity.java
@@ -3,11 +3,32 @@ package com.example.songhui.bottomnavigationbardemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ashokvarma.bottomnavigation.BadgeItem;
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationBar bottomNavigationBar;
+    BadgeItem numberBadgeItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationBar = (BottomNavigationBar)findViewById(R.id.bottom_navigation_bar);
+        numberBadgeItem = new BadgeItem()
+                .setBorderWidth(4)
+                .setBackgroundColorResource(R.color.blue)
+                .setText("" + 0)
+                .setHideOnSelect(true);
+        bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.drawable.ic_home_white_24dp, "Home").setActiveColorResource(R.color.orange).setBadgeItem(numberBadgeItem))
+                .addItem(new BottomNavigationItem(R.drawable.ic_book_white_24dp, "Books").setActiveColorResource(R.color.teal))
+                .addItem(new BottomNavigationItem(R.drawable.ic_music_note_white_24dp, "Music").setActiveColorResource(R.color.blue))
+                .addItem(new BottomNavigationItem(R.drawable.ic_tv_white_24dp, "Movies & TV").setActiveColorResource(R.color.brown))
+                .addItem(new BottomNavigationItem(R.drawable.ic_videogame_asset_white_24dp, "Games").setActiveColorResource(R.color.grey))
+                .setFirstSelectedPosition(0)
+                .initialise();
     }
 }
